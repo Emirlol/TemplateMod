c2528b2b9447d18f95f301487a1d94e871ce6004
Rime <81419447+Emirlol@users.noreply.github.com>
2024-05-11 22:38:02 +0300

    Rename ConfigScreenFactory into ModMenuApiImpl

--- src/main/kotlin/me/lumiafk/templatemod/modmenu/ModMenuApiImpl.kt
+++ src/main/kotlin/me/lumiafk/templatemod/modmenu/ModMenuApiImpl.kt
@@ -4,6 +4,6 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import com.terraformersmc.modmenu.api.ModMenuApi
 import me.lumiafk.templatemod.config.TemplateConfigHandler
 
-class ConfigScreenFactory: ModMenuApi {
+class ModMenuApiImpl: ModMenuApi {
     override fun getModConfigScreenFactory() = ConfigScreenFactory(TemplateConfigHandler::createGui)
 }
