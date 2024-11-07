8e20d667b1f04c761517c210fa94ee977010a0f6
Rime <81419447+Emirlol@users.noreply.github.com>
2024-11-07 23:14:01 +0300

    Switch to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-	id("fabric-loom") version "1.8-SNAPSHOT"
-	kotlin("jvm") version "2.0.21"
+	alias(libs.plugins.loom)
+	alias(libs.plugins.kotlin)
+	alias(libs.plugins.modPublish)
 }
 
 repositories {
@@ -13,47 +14,38 @@ repositories {
 	}
 }
 
-val minecraftVersion = property("minecraft_version") as String
-val yarnMappings = property("yarn_mappings") as String
-val loaderVersion = property("fabric_loader_version") as String
-
 val modName = property("mod_name") as String
 val modId = property("mod_id") as String
-version = property("mod_version") as String
 group = property("maven_group") as String
-
-val fabricApiVersion = property("fabric_api_version") as String
-val fabricKotlinVersion = property("fabric_kotlin_version") as String
-val yaclVersion = property("yacl_version") as String
-val modmenuVersion = property("modmenu_version") as String
+version = "${libs.versions.modVersion.get()}+${libs.versions.minecraft.get()}"
 
 dependencies {
-	// To change the versions see the gradle.properties file
-	minecraft("com.mojang:minecraft:$minecraftVersion")
-	mappings("net.fabricmc:yarn:$yarnMappings:v2")
-	modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+	// To change the versions, see the gradle.properties file
+	minecraft(libs.minecraft)
+	mappings(libs.yarn)
+	modImplementation(libs.fabricLoader)
 
-	modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricApiVersion")
-	modImplementation("net.fabricmc:fabric-language-kotlin:$fabricKotlinVersion")
-	modImplementation("dev.isxander:yet-another-config-lib:$yaclVersion")
-	modImplementation("com.terraformersmc:modmenu:$modmenuVersion")
+	modImplementation(libs.fabricApi)
+	modImplementation(libs.fabricLanguageKotlin)
+	modImplementation(libs.yacl)
+	modImplementation(libs.modMenu)
 }
 
 tasks {
 	processResources {
+		val props = mapOf(
+			"name" to modName,
+			"mod_id" to modId,
+			"version" to version,
+			"minecraft_version" to libs.versions.minecraft.get(),
+			"loader_version" to libs.versions.fabricLoader.get(),
+			"fabric_kotlin_version" to libs.versions.fabricLanguageKotlin.get(),
+			"modmenu_version" to libs.versions.modMenu.get(),
+			"yacl_version" to libs.versions.yacl.get()
+		)
+		inputs.properties(props)
 		filesMatching("fabric.mod.json") {
-			expand(
-				mapOf(
-					"version" to version,
-					"minecraft_version" to minecraftVersion,
-					"loader_version" to loaderVersion,
-					"name" to modName,
-					"mod_id" to modId,
-					"fabric_kotlin_version" to fabricKotlinVersion,
-					"modmenu_version" to modmenuVersion,
-					"yacl_version" to yaclVersion
-				)
-			)
+			expand(props)
 		}
 		filesMatching("assets/$modId/lang/*.json") {
 			expand(
@@ -61,6 +53,11 @@ tasks {
 					"namespace" to modId
 				)
 			)
+		}
+	}
+	jar {
+		from("LICENSE") {
+			rename { "${it}_${base.archivesName.get()}"}
 		}
 	}
 }
